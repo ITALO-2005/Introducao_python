b4653c5605c75d853c6b8bd7ebf7b32ef960bb19
ITALO-2005 <italo-santos.is@academico.ifpb.edu.br>
2024-08-09 17:36:29 +0000

    teste2

--- helloworld.py
+++ helloworld.py
@@ -1,2 +1,13 @@
-print('Hello World')
-print("World")
+print('Ítalo Dantas Santos')
+
+#Diferença entre linguagens formais e naturais:
+
+#Linguagens Formais: Usadas em matemática, lógica e programação, seguem regras rigorosas e precisas, sem espaço para ambiguidades. Exemplo: Python, Java.
+
+#Linguagens Naturais: São os idiomas falados pelas pessoas, como português e inglês. São flexíveis e podem ser ambíguas, permitindo múltiplas interpretações.
+
+#Três características que distinguem Python:
+
+#Sintaxe Simples e Clara: Facilita a leitura e escrita de código.
+#Tipagem Dinâmica: Não exige declaração explícita de tipos de dados.
+#Bibliotecas Amplas: Possui uma vasta coleção de bibliotecas prontas para uso em diversas áreas.
